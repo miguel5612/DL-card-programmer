aa9300591f0f070b2a1edcecb6b0a9870b87848d
miguel5612 <miguelangelcu@ufps.edu.co>
2018-12-08 18:16:49 -0500

    pFillPortListe OK

--- DLCardProgrammer/DLCardProgrammer/Form1.Designer.cs
+++ DLCardProgrammer/DLCardProgrammer/Form1.Designer.cs
@@ -34,18 +34,19 @@
             this.cmbPort = new System.Windows.Forms.ComboBox();
             this.cmbBaudrate = new System.Windows.Forms.ComboBox();
             this.btnLoadProgram = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnViewCode = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.ddTypeProgram = new System.Windows.Forms.ComboBox();
+            this.cmbTypeProgram = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnProgram = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblPort
             // 
             this.lblPort.AutoSize = true;
-            this.lblPort.Location = new System.Drawing.Point(12, 30);
+            this.lblPort.Location = new System.Drawing.Point(15, 18);
             this.lblPort.Name = "lblPort";
             this.lblPort.Size = new System.Drawing.Size(52, 13);
             this.lblPort.TabIndex = 0;
@@ -55,7 +56,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Location = new System.Drawing.Point(15, 46);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 13);
             this.label2.TabIndex = 1;
@@ -64,7 +65,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Location = new System.Drawing.Point(15, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(117, 13);
             this.label1.TabIndex = 2;
@@ -73,7 +74,7 @@
             // cmbPort
             // 
             this.cmbPort.FormattingEnabled = true;
-            this.cmbPort.Location = new System.Drawing.Point(147, 27);
+            this.cmbPort.Location = new System.Drawing.Point(150, 15);
             this.cmbPort.Name = "cmbPort";
             this.cmbPort.Size = new System.Drawing.Size(121, 21);
             this.cmbPort.TabIndex = 3;
@@ -81,85 +82,94 @@
             // cmbBaudrate
             // 
             this.cmbBaudrate.FormattingEnabled = true;
-            this.cmbBaudrate.Location = new System.Drawing.Point(147, 55);
+            this.cmbBaudrate.Location = new System.Drawing.Point(150, 43);
             this.cmbBaudrate.Name = "cmbBaudrate";
             this.cmbBaudrate.Size = new System.Drawing.Size(121, 21);
             this.cmbBaudrate.TabIndex = 4;
             // 
             // btnLoadProgram
             // 
-            this.btnLoadProgram.Location = new System.Drawing.Point(147, 82);
+            this.btnLoadProgram.Location = new System.Drawing.Point(150, 70);
             this.btnLoadProgram.Name = "btnLoadProgram";
             this.btnLoadProgram.Size = new System.Drawing.Size(121, 23);
             this.btnLoadProgram.TabIndex = 5;
             this.btnLoadProgram.Text = "Cargar programa";
             this.btnLoadProgram.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnViewCode
             // 
-            this.button1.Location = new System.Drawing.Point(147, 111);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Ver el codigo";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnViewCode.Location = new System.Drawing.Point(150, 99);
+            this.btnViewCode.Name = "btnViewCode";
+            this.btnViewCode.Size = new System.Drawing.Size(121, 23);
+            this.btnViewCode.TabIndex = 6;
+            this.btnViewCode.Text = "Ver el codigo";
+            this.btnViewCode.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 116);
+            this.label3.Location = new System.Drawing.Point(15, 104);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(110, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "VER EL PROGRAMA";
             // 
-            // ddTypeProgram
+            // cmbTypeProgram
             // 
-            this.ddTypeProgram.FormattingEnabled = true;
-            this.ddTypeProgram.Location = new System.Drawing.Point(147, 140);
-            this.ddTypeProgram.Name = "ddTypeProgram";
-            this.ddTypeProgram.Size = new System.Drawing.Size(121, 21);
-            this.ddTypeProgram.TabIndex = 8;
+            this.cmbTypeProgram.FormattingEnabled = true;
+            this.cmbTypeProgram.Location = new System.Drawing.Point(150, 128);
+            this.cmbTypeProgram.Name = "cmbTypeProgram";
+            this.cmbTypeProgram.Size = new System.Drawing.Size(121, 21);
+            this.cmbTypeProgram.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 143);
+            this.label4.Location = new System.Drawing.Point(15, 131);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(102, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "FORMA DE ENVIO ";
             // 
-            // button2
+            // btnProgram
             // 
-            this.button2.Location = new System.Drawing.Point(147, 167);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Programar Tarjeta";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnProgram.Location = new System.Drawing.Point(150, 155);
+            this.btnProgram.Name = "btnProgram";
+            this.btnProgram.Size = new System.Drawing.Size(121, 23);
+            this.btnProgram.TabIndex = 10;
+            this.btnProgram.Text = "Programar Tarjeta";
+            this.btnProgram.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 172);
+            this.label5.Location = new System.Drawing.Point(15, 160);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(77, 13);
             this.label5.TabIndex = 11;
             this.label5.Text = "PROGRAMAR";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(311, 18);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(246, 20);
+            this.txtName.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.ClientSize = new System.Drawing.Size(547, 202);
+            this.ClientSize = new System.Drawing.Size(585, 205);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnProgram);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.ddTypeProgram);
+            this.Controls.Add(this.cmbTypeProgram);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnViewCode);
             this.Controls.Add(this.btnLoadProgram);
             this.Controls.Add(this.cmbBaudrate);
             this.Controls.Add(this.cmbPort);
@@ -181,12 +191,13 @@
         private System.Windows.Forms.ComboBox cmbPort;
         private System.Windows.Forms.ComboBox cmbBaudrate;
         private System.Windows.Forms.Button btnLoadProgram;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnViewCode;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox ddTypeProgram;
+        private System.Windows.Forms.ComboBox cmbTypeProgram;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnProgram;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
     }
 }
 
--- DLCardProgrammer/DLCardProgrammer/Form1.cs
+++ DLCardProgrammer/DLCardProgrammer/Form1.cs
@@ -7,20 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Globalization;
+using System.Text;
+
 
 namespace DLCardProgrammer
 {
     public partial class Form1 : Form
     {
-        private static string _program = ""
+        public string _programa = string.Empty;
+        SerialPort ArduinoPort = new SerialPort();
         public Form1()
         {
             InitializeComponent();
+            //Al llamar a esta funcion se llena la lista de puertos:
+            pLoadSerialPorts();
+
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+        private void pLoadSerialPorts()
+        {
+            var countPorts = 0;
+            cmbPort.Items.Clear();
+            foreach (String port in SerialPort.GetPortNames())
+            {
+                cmbPort.Items.Add(port);
+                countPorts++;
+            }
+            if (countPorts == 0) cmbPort.Items.Add("COM3");
+            cmbPort.SelectedIndex = 0;
         }
     }
 }
